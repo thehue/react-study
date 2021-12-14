12810bb220a7603f0d44b6154e13ef7541a8fb7d
thehue <gouk32@naver.com>
2021-12-14 21:15:17 +0900

    리액트 기본 사용법 익히기

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+/* @jsx React.createElement */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Button({ children }) {
+  return <button type="button">{children}</button>;
+}
+
+function Buttons() {
+  return (
+    <p>
+      {
+        [1, 2, 3].map((i) => <Button key={i}>{i}</Button>)
+      }
+    </p>
+  );
+}
+
+function App() {
+  return <div>
+    <Buttons />
+  </div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
